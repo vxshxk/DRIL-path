fb6c4c6f63ac928e0bc9a9b786e3edd664f5153c
Vishak Kamath <115456558+vxshxk@users.noreply.github.com>
2023-11-09 01:55:20 +0530

    Add files via upload

--- driver.cpp
+++ driver.cpp
@@ -6,11 +6,11 @@ using namespace std;
 #include "stack.h"
 #include "stack_impl.cpp"
 int main()
-{   
-    Array a;
+{
+    Stack a;
     a += 1;
     a += 2;
-    Array a1;
+    Stack a1;
     a1 += 1;
     a1 += 3;
     a1 << 0; // Display the elements in a1
@@ -20,20 +20,5 @@ int main()
     a << 0; // Display the elements in a
     a1 -= 3; // Remove an element from a1
     a1 << 0; // Display the elements in a1
-
-    
-    Stack s;
-    s += 1;
-    s += 2;
-    Stack s1;
-    s1 += 1;
-    s1 += 3;
-    s1 << 0; // Display the elements in a1
-    cout<<endl; // Check if a and a1 are equal or not
-    cout << "s == s1: " << (s == s1) << endl;
-    cout << "s != s1: " << (s != s1) << endl;
-    s << 0; // Display the elements in a
-    s1 -= 3; // Remove an element from a1
-    s1 << 0; // Display the elements in a1
     return 0;
 }
